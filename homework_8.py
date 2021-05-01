0bad6648cd21fa67c75efc45ab88d3cce0f918ce
Alexander Degtiarov <alexander.degtiarov96@gmail.com>
2021-05-01 16:33:43 +0300

    Часы v1

---
+++ homework_8.py
@@ -0,0 +1,82 @@
+"""
+Сделать часы, используя:
+open, import, и всякое другое
+"""
+
+import support_homework_8
+from support_homework_8 import black, white
+from support_homework_8 import num_storage
+import time
+from datetime import datetime
+import os
+
+black = "\u2B1B"
+white = "\u2B1C"
+
+
+# Функция при вызове отдаёт время словарём чисел
+# возможно зря
+def time_now():
+    a = datetime.now()
+    hour = a.strftime("%H")
+    minutes = a.strftime("%M")
+    seconds = a.strftime("%S")
+    return int(hour), int(minutes), int(seconds)
+
+
+# Генератор
+def generator():
+    command = {1: white, 2: black}
+    for elem in command:
+        yield command[elem]
+
+
+# собирает словарь с теперешним временем для последующего перебора
+# и вывода в цикле
+def some_func():
+    led = generator()
+    answer = {0: [], 1: [], 2: [], 3: [], 4: [], 5: [], 6: []}
+    count_1 = 0
+    if time_now()[2] % 2 == 0:
+        now_led = black
+    else:
+        now_led = white
+    for i in time_now():
+        first = i//10
+        second = i%10
+        for elem in num_storage[first]:
+            answer[elem].append(num_storage[first][elem])
+        for elem in answer:
+            answer[elem].append([black])
+        for elem in num_storage[second]:
+            answer[elem].append(num_storage[second][elem])
+
+        if count_1 != 2:
+            for elem in answer:
+                answer[elem].append([black])
+
+            for elem in answer:
+                if elem == 2 or elem == 4:
+                    answer[elem].append(now_led)
+                else:
+                    answer[elem].append([black])
+
+            for elem in answer:
+                answer[elem].append([black])
+            
+            count_1 += 1
+    return answer
+
+
+
+   
+for a in range(10):
+    os.system("cls")
+    answer = some_func()
+    for i in answer:
+        for elem in answer[i]:
+            for item in elem:
+                print(item, end="")
+        print()
+    time.sleep(0.25)
+    
---
+++ support_homework_8.py
@@ -0,0 +1,109 @@
+import random
+
+
+black = "\u2B1B"
+white = "\u2B1C"
+
+
+num_storage = {
+    0: {
+        0: [black, white, white, black],
+        1: [white, black, black, white],
+        2: [white, black, black, white],
+        3: [white, black, black, white],
+        4: [white, black, black, white],
+        5: [white, black, black, white],
+        6: [black, white, white, black]
+    },
+    1: {
+        0: [black, black, black, white],
+        1: [black, black, black, white],
+        2: [black, black, black, white],
+        3: [black, black, black, white],
+        4: [black, black, black, white],
+        5: [black, black, black, white],
+        6: [black, black, black, white]
+    },
+    2: {
+        0: [white, white, white, white],
+        1: [black, black, black, white],
+        2: [black, black, black, white],
+        3: [white, white, white, white],
+        4: [white, black, black, black],
+        5: [white, black, black, black],
+        6: [white, white, white, white]
+    },
+    3: {
+        0: [white, white, white, white],
+        1: [black, black, black, white],
+        2: [black, black, black, white],
+        3: [white, white, white, white],
+        4: [black, black, black, white],
+        5: [black, black, black, white],
+        6: [white, white, white, white]
+    },
+    4: {
+        0: [white, black, black, white],
+        1: [white, black, black, white],
+        2: [white, black, black, white],
+        3: [white, white, white, white],
+        4: [black, black, black, white],
+        5: [black, black, black, white],
+        6: [black, black, black, white]
+    },
+    5: {
+        0: [white, white, white, white],
+        1: [white, black, black, black],
+        2: [white, black, black, black],
+        3: [white, white, white, white],
+        4: [black, black, black, white],
+        5: [black, black, black, white],
+        6: [white, white, white, white]
+    },
+    6: {
+        0: [white, white, white, white],
+        1: [white, black, black, black],
+        2: [white, black, black, black],
+        3: [white, white, white, white],
+        4: [white, black, black, white],
+        5: [white, black, black, white],
+        6: [white, white, white, white]
+    },
+    7: {
+        0: [white, white, white, white],
+        1: [black, black, black, white],
+        2: [black, black, black, white],
+        3: [black, black, black, white],
+        4: [black, black, black, white],
+        5: [black, black, black, white],
+        6: [black, black, black, white]
+    },
+    8: {
+        0: [black, white, white, black],
+        1: [white, black, black, white],
+        2: [white, black, black, white],
+        3: [white, white, white, white],
+        4: [white, black, black, white],
+        5: [white, black, black, white],
+        6: [black, white, white, black]
+    },
+    9: {
+        0: [white, white, white, white],
+        1: [white, black, black, white],
+        2: [white, black, black, white],
+        3: [white, white, white, white],
+        4: [black, black, black, white],
+        5: [black, black, black, white],
+        6: [white, white, white, white]
+    }
+}
+
+
+if __name__ == "__main__":
+    for elem in numbers:
+        for i in numbers[elem]:
+            answer = ""
+            for a in numbers[elem][i]:
+                answer += a
+            print(answer)
+        print()
